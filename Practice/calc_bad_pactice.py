382b0c70ed02dc7a778681d055dbab117e37fd6a
NSOLAf1 <nsolaf1@gmail.com>
2025-01-28 14:36:33 +0600

    l4

--- Practice/calc_bad_pactice.py
+++ Practice/calc_bad_pactice.py
@@ -1,32 +1,34 @@
-def calculate():
-    print("Выберите операцию: ")
-    print("1: Сложение (+)")
-    print("2: Вычитание (-)")
-    print("3: Умножение (*)")
-    print("4: Деление (/)")
 
-    choice=int(input("Введите номер операции: "))
+print("Выберите операцию: ")
+print("1: Сложение (+)")
+print("2: Вычитание (-)")
+print("3: Умножение (*)")
+print("4: Деление (/)")
 
-    if choice== 1:
-        num1=float(input("Введите первое число: "))
-        num2=float(input("Введите второе число: "))
-        print(num1+num2)
-    elif choice== "2":
-        num1=float(input("Введите первое число: "))
-        num2=float(input("Введите второе число: "))
-        print(num1-num2)
-    elif choice== "3":
-        num1=float(input("Введите первое число: "))
-        num2=float(input("Введите второе число: "))
-        print(num1*num2)  
-    elif choice== "4":
-        num1=float(input("Введите первое число: "))
-        num2=float(input("Введите второе число: "))
-        if num2 !=0:
-            print(num1 /num2)
-        else:
-            print("Делить на 0 нельзя")
+choice=int(input("Введите номер операции: "))
+
+if choice== 1:
+    num1=float(input("Введите первое число: "))
+    num2=float(input("Введите второе число: "))
+    print(num1+num2)
+elif choice== "2":
+    num1=float(input("Введите первое число: "))
+    num2=float(input("Введите второе число: "))
+    print(num1-num2)
+elif choice== "3":
+    num1=float(input("Введите первое число: "))
+    num2=float(input("Введите второе число: "))
+    print(num1*num2)  
+elif choice== "4":
+    num1=float(input("Введите первое число: "))
+    num2=float(input("Введите второе число: "))
+    if num2 !=0:
+        print(num1 /num2)
     else:
-        print("Такого варианта нет!")
+        print("Делить на 0 нельзя")
+else:
+    print("Такого варианта нет!")
 
-calculate()
+
+
+num1 = (4-3)* 5
